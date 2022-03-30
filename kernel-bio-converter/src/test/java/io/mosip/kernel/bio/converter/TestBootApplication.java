b8121ce83f157c26174def8675fc5f0684e7aff3
JanardhanBS-SyncByte <janardhan@syncbyte.in>
2022-03-30 16:48:30 +0530

    Changes related to review comments.
    Signed-off-by: ckm007 <chandrakeshavmishra@gmail.com>

--- kernel-bio-converter/src/test/java/io/mosip/kernel/bio/converter/TestBootApplication.java
+++ kernel-bio-converter/src/test/java/io/mosip/kernel/bio/converter/TestBootApplication.java
@@ -1,12 +1,16 @@
 package io.mosip.kernel.bio.converter;
 
+import io.mosip.kernel.bio.converter.config.TestSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication (exclude= {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication (scanBasePackages = {"${mosip.auth.adapter.impl.basepackage}, io.mosip.kernel.bio.*"})
+@EnableAsync
+@EnableCaching
+@Import(TestSecurityConfig.class)
 public class TestBootApplication  {
 	public static void main(String[] args) {
 		SpringApplication.run(TestBootApplication.class, args);
