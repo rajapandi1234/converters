db55bb200467ea06b06e591ffeebd8b3df97bd90
JanardhanBS-SyncByte <janardhan@syncbyte.in>
2022-09-23 14:04:37 +0530

    made changes to use as a library

--- kernel-bio-converter/src/main/java/io/mosip/kernel/bio/converter/KernelBioConverterApplication.java
+++ kernel-bio-converter/src/main/java/io/mosip/kernel/bio/converter/KernelBioConverterApplication.java
@@ -1,9 +1,11 @@
 package io.mosip.kernel.bio.converter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication (scanBasePackages = {"${mosip.auth.adapter.impl.basepackage}, io.mosip.kernel.bio.*"})
+@EnableAutoConfiguration
 public class KernelBioConverterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KernelBioConverterApplication.class, args);
